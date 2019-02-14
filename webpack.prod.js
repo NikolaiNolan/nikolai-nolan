1d1b2866f63704afe1d2dd7e1b474d5ce516bb80
Nikolai Nolan <atlassian@nikol.ai>
2019-02-13 22:37:17 -0600

    Add responsive images

--- webpack.prod.js
+++ webpack.prod.js
@@ -47,7 +47,7 @@ module.exports = {
       },
       {
         // Load all images as base64 encoding if they are smaller than 8192 bytes
-        test: /\.(png|jpg|gif)$/,
+        test: /\.(png|jpg|webp|gif)$/,
         use: [
           {
             loader: 'url-loader',
